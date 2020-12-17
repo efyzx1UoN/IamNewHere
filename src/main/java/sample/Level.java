b05431ce5580224827554f83e40bd5fa22d225b0
efyzx1 <efyzx1@nottingham.ac.uk>
2020-12-17 15:08:09 +0800

    If applied,this commit will update javadocs and bob code

--- src/main/java/sample/Level.java
+++ src/main/java/sample/Level.java
@@ -9,7 +9,11 @@ import java.util.List;
 
 import static sample.GameGrid.translatePoint;
 
-
+/**
+ * @author Zihui xu - Modified
+ *  @version 1.0
+ *  Setting and Getting GameObjects into Level
+ */
 public final class Level implements Iterable<GameObject> {
 
     private final String name;
@@ -19,6 +23,12 @@ public final class Level implements Iterable<GameObject> {
     private int numberOfDiamonds = 0;
     private Point keeperPosition = new Point(0, 0);
 
+    /**
+     * Level issue
+     * @param levelName is String Object
+     * @param levelIndex is Integer Object
+     * @param raw_level is List Object
+     */
     public Level(String levelName, int levelIndex, List<String> raw_level) {
         if (StartMeUp.isDebugActive()) {
             System.out.printf("[ADDING LEVEL] LEVEL [%d]: %s\n", levelIndex, levelName);
@@ -41,7 +51,8 @@ public final class Level implements Iterable<GameObject> {
             // http://stackoverflow.com/questions/8894258/fastest-way-to-iterate-over-all-the-chars-in-a-string
             for (int col = 0; col < raw_level.get(row).length(); col++) {
                 // The game object is null when the we're adding a floor or a diamond
-                GameObject curTile = GameObject.fromChar(raw_level.get(row).charAt(col));
+                GameObject curTile = GameObject.fromChar(raw_level.
+                        get(row).charAt(col));
 
                 if (curTile == GameObject.DIAMOND) {
                     numberOfDiamonds++;
@@ -57,11 +68,17 @@ public final class Level implements Iterable<GameObject> {
         }
     }
 
+    /**
+     * Check if is complete
+     * @return
+     */
     boolean isComplete() {
         int cratedDiamondsCount = 0;
         for (int row = 0; row < objectsGrid.ROWS; row++) {
             for (int col = 0; col < objectsGrid.COLUMNS; col++) {
-                if (objectsGrid.getGameObjectAt(col, row) == GameObject.CRATE && diamondsGrid.getGameObjectAt(col, row) == GameObject.DIAMOND) {
+                if (objectsGrid.getGameObjectAt(col, row) == GameObject.
+                        CRATE && diamondsGrid.
+                        getGameObjectAt(col, row) == GameObject.DIAMOND) {
                     cratedDiamondsCount++;
                 }
             }
@@ -70,36 +87,75 @@ public final class Level implements Iterable<GameObject> {
         return cratedDiamondsCount >= numberOfDiamonds;
     }
 
+    /**
+     * return name
+     * @return
+     */
     public String getName() {
         return name;
     }
 
+    /**
+     * return index
+     * @return
+     */
     int getIndex() {
         return index;
     }
 
+    /**
+     * return keeperPosition
+     * @return
+     */
     Point getKeeperPosition() {
         return keeperPosition;
     }
 
+    /**
+     * return TargetFromSource
+     * @param source is point object
+     * @param delta is point object
+     * @return
+     */
     GameObject getTargetObject(Point source, Point delta) {
         return objectsGrid.getTargetFromSource(source, delta);
     }
 
+    /**
+     * return GameObjects
+     * @param p is point object
+     * @return
+     */
     GameObject getObjectAt(Point p) {
         return objectsGrid.getGameObjectAt(p);
     }
 
+    /**
+     * moveGameObject to somewhere
+     * @param object
+     * @param source is point object
+     * @param delta is point object
+     */
     void moveGameObjectBy(GameObject object, Point source, Point delta) {
         moveGameObjectTo(object, source, translatePoint(source, delta));
     }
 
-    public void moveGameObjectTo(GameObject object, Point source, Point destination) {
+    /**
+     * moveGameObject to destination
+     * @param object
+     * @param source is point object
+     * @param destination is point object
+     */
+    public void moveGameObjectTo(GameObject object, Point source,
+                                 Point destination) {
         objectsGrid.putGameObjectAt(getObjectAt(destination), source);
         objectsGrid.putGameObjectAt(object, destination);
         
     }
-    
+
+    /**
+     *   get point step
+     */
     class Step{
     	GameGrid objectGrid;
     	Point keeperPoint;
@@ -108,25 +164,38 @@ public final class Level implements Iterable<GameObject> {
     		keeperPoint=new Point(point);
     		copyGrid(gameGrid);
 		}
-    	
+
+        /**
+         *  return Game Object
+         * @param gameGrid
+         */
     	private void copyGrid(GameGrid gameGrid) {
     		int columns=gameGrid.COLUMNS;
     		int rows=gameGrid.ROWS;
 			objectGrid=new GameGrid(columns, rows);
 			for (int i = 0; i < rows; i++) {
 				for (int j = 0; j < columns; j++) {
-					objectGrid.putGameObjectAt(gameGrid.getGameObjectAt(i, j), i, j);
+					objectGrid.putGameObjectAt(gameGrid.
+                            getGameObjectAt(i, j), i, j);
 				}
 			}
 		}
     }
-    
+
+
     private ArrayList<Step> steps=new ArrayList<Level.Step>();
-    
+
+    /**
+     * add step
+     */
     public void logMove() {
 		steps.add(0,new Step(objectsGrid, keeperPosition));
 	}
-    
+
+    /**
+     * undo
+     * go back to last step
+     */
     public void undo() {
     	if (steps.isEmpty()) {
 			return;
@@ -136,16 +205,27 @@ public final class Level implements Iterable<GameObject> {
 		keeperPosition=step.keeperPoint;
 	}
 
+    /**
+     * ToString  Method
+     * @return
+     */
     @Override
     public String toString() {
         return objectsGrid.toString();
     }
 
+    /**
+     * Iterator GameObject
+     * @return
+     */
     @Override
     public Iterator<GameObject> iterator() {
         return new LevelIterator();
     }
 
+    /**
+     *return new point
+     */
     public class LevelIterator implements Iterator<GameObject> {
 
         int column = 0;
@@ -153,7 +233,8 @@ public final class Level implements Iterable<GameObject> {
 
         @Override
         public boolean hasNext() {
-            return !(row == objectsGrid.ROWS - 1 && column == objectsGrid.COLUMNS);
+            return !(row == objectsGrid.ROWS - 1 &&
+                    column == objectsGrid.COLUMNS);
         }
 
         @Override
